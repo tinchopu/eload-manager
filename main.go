ce1fe8f3972207e2f3a5c20bd33701094e155cd1
Anton Puetz <anton@amp-digital.de>
2022-07-22 10:25:12 +0200

    Added default route

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hi",
+		})
+	})
 	router.GET("/complete/:currentPercentage", calculateTimeToBeAt80Percent)
 	router.Run()
 }
